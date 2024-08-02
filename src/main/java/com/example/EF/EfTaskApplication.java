eb1cf8ad3b7ced4d49a878937724bd76fa8447e8
Syrgak2 <kary6ekov.s@gmail.com>
2024-08-02 22:23:28 +0600

    created project skeleton

---
+++ src/main/java/com/example/EF/EfTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.EF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EF/EfTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
